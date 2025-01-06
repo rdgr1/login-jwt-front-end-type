f331eddaa2be9a4a098761bec743cfb909317a84
Rodger <rvidaldias@gmail.com>
2025-01-06 13:57:27 -0300

    init creation of form login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { DefaultLayoutLoginComponent } from "../../components/default-layout-login/default-layout-login.component";
-
+import{FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms"
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultLayoutLoginComponent],
+  imports: [DefaultLayoutLoginComponent,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  loginForm!: FormGroup;
+    constructor(){
+      this.loginForm = new FormGroup({
+        email: new FormControl('',[Validators.required,Validators.email]),
+        password: new FormControl('',[Validators.required,Validators.minLength(6)])
+      })
+    }
 }
